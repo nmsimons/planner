98a814f33e9945389d661e12d689d30c249e6a52
Josmithr <josmithr@microsoft.com>
2024-07-19 13:28:33 -0700

    refactor: Use zod

--- src/utils/gpt_helpers.ts
+++ src/utils/gpt_helpers.ts
@@ -1,21 +1,19 @@
-import { v4 as uuid } from "uuid";
-import { createAzureOpenAILanguageModel, createJsonTranslator } from "typechat";
-import { getJsonSchema } from "@fluidframework/tree/internal";
-import { Session, Sessions } from "../schema/app_schema.js";
-import Ajv from "ajv";
 import { InsertableTypedNode } from "fluid-framework";
+import { createAzureOpenAILanguageModel, createJsonTranslator } from "typechat";
+import { getZodSchemaAsTypeScript } from "typechat/zod";
+import { ZodType } from "zod";
+import { getZodSchema } from "@fluidframework/tree/internal";
 
-const sessionsJsonSchema = getJsonSchema(Sessions);
-const jsonValidator = new Ajv.default({ strict: false });
-const sessionsValidator = jsonValidator.compile(sessionsJsonSchema);
+import { Session, Sessions } from "../schema/app_schema.js";
 
+const sessionsZodSchema = getZodSchema(Sessions) as ZodType<InsertableTypedNode<typeof Sessions>>;
+
+// Note: until tree schema supports literal unions, we need to explain what "sessionType" is.
 const sessionSystemPrompt = `You are a service named Copilot that takes a user prompt and generates session topics for a "speaking event" scheduling application.
 The "sessionType" is a string that indicates the type of the session. It can be one of 'session', 'keynote', 'panel', or 'workshop'.
 `;
 
-export function createSessionPrompter(): (
-	prompt: string,
-) => Promise<Iterable<Session> | undefined> {
+export function createSessionPrompter(): (prompt: string) => Promise<Iterable<Session>> {
 	const endpoint =
 		process.env.AZURE_OPENAI_ENDPOINT ?? localStorage.getItem("AZURE_OPENAI_ENDPOINT");
 
@@ -33,52 +31,56 @@ export function createSessionPrompter(): (
 	}
 
 	const model = createAzureOpenAILanguageModel(apiKey, endpoint);
+
+	const sessionsTypeScript = getZodSchemaAsTypeScript({
+		[Sessions.identifier]: sessionsZodSchema,
+	});
+	console.log("Generated schema:\n", sessionsTypeScript);
+
 	const translator = createJsonTranslator<Sessions>(model, {
 		getTypeName: () => Sessions.identifier,
-		getSchemaText: () => JSON.stringify(sessionsJsonSchema),
-		validate(jsonObject: Record<string, unknown>) {
-			const sessionsJsonObject = jsonObject[Sessions.identifier];
-			if (sessionsValidator(sessionsJsonObject)) {
+		getSchemaText: () => JSON.stringify(sessionsTypeScript),
+		validate(response: Record<string, unknown>) {
+			const sessionsJsonObject = response[Sessions.identifier];
+			console.log("Response: ", sessionsJsonObject);
+			try {
+				const typed = sessionsZodSchema.parse(sessionsJsonObject);
 				console.log("Response passed validation!");
 				return {
 					success: true,
-					data: new Sessions(
-						sessionsJsonObject as Iterable<InsertableTypedNode<typeof Session>>,
-					),
+					data: new Sessions(typed as Iterable<InsertableTypedNode<typeof Session>>),
+				};
+			} catch (error: any) {
+				console.error("Response was malformed.");
+				console.error(error.message);
+				return {
+					success: false,
+					message: "Malformed generated Sessions",
 				};
 			}
-
-			console.error(
-				"Response was malformed.",
-				sessionsValidator.errors?.map((e) => JSON.stringify(e)),
-			);
-			return {
-				success: false,
-				message: "Malformed generated Sessions",
-			};
 		},
 	});
 
-	return async (prompt) => {
-		try {
-			const result = await translator.translate(prompt, sessionSystemPrompt);
-			if (!result.success) {
-				throw new Error("AI did not return result");
-			}
-			const sessions: Session[] = result.data.map((l) => {
-				const currentTime = new Date().getTime();
-				return new Session({
-					title: l.title,
-					abstract: l.abstract,
-					created: currentTime,
-					sessionType: l.sessionType,
-					lastChanged: currentTime,
-					id: uuid(),
-				});
-			});
-			return sessions;
-		} catch (e) {
-			return undefined;
+	return async (prompt): Promise<Iterable<Session>> => {
+		const result = await translator.translate(prompt, sessionSystemPrompt);
+		if (!result.success) {
+			throw new Error(`AI did not return a valid response. Error: "${result.message}".`);
 		}
+		// TODO: separate schema for core "Session" info and annotation info like timestamps.
+		// We don't need the LLM to generate the other information.
+		// `id` is another interesting case. We don't really need an LLM to generate it, but we do want it to be
+		// a part of the "core" schema.
+		// Maybe whether or not "identifiers" are part of derived schemas is a supported policy option?
+		const sessions: Session[] = result.data.map((l) => {
+			const currentTime = new Date().getTime();
+			return new Session({
+				title: l.title,
+				abstract: l.abstract,
+				created: currentTime,
+				sessionType: l.sessionType,
+				lastChanged: currentTime,
+			});
+		});
+		return sessions;
 	};
 }
