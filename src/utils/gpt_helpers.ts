b8a9e5257cb1a41cc665f3d1b8c1537d6bf045ea
Joshua Smithrud <54606601+Josmithr@users.noreply.github.com>
2024-09-17 18:10:28 +0000

    Revert "revert: Restore examples"
    This reverts commit 6589273675505bd7ec5ee48c976645e3d4d35b2f.

--- src/utils/gpt_helpers.ts
+++ src/utils/gpt_helpers.ts
@@ -14,33 +14,6 @@ const sessionsValidator = jsonValidator.compile(sessionsJsonSchema);
 
 const sessionSystemPrompt = `You are a service named Copilot that takes a user prompt and generates session topics for a "speaking event" scheduling application.
 The "sessionType" is a string that indicates the type of the session. It can be one of 'session', 'keynote', 'panel', or 'workshop'.
-For example, if a user asks for three lectures about green energy, you might output:
-{
-	"title": "Wind Power",
-	"abstract": "Dr. Alexander Pardes provides an analysis of the latest wind turbine designs and how they've improved upon existing technologies.",
-	"sessionType": "session"
-},
-{
-	"title": "Solar Complacency",
-	"abstract": "Recent trends in solar panel efficiency point to a future of diminishing returns. How can we encourage new ideas in a competitive engineering space?",
-	"sessionType": "session"
-},
-{
-	"title": "Exploring Deeper: Geothermal Energy with a Twist",
-	"abstract": "Several leading scientists discuss how we can tap the pressure differentials in the earth's crust to generate 'friction-energy', a technique that has only recently moved beyond pure theoretical speculation.",
-	"sessionType": "session"
-}
-Or, another example, if a user asks for two lectures about raccoons where one is a keynote, you might output:
-{
-	"title": "Furry Friends or Furious Foes?",
-	"abstract": "Raccoon banditry is on the rise and homeowners aren't happy. However, with a few adjustments to our behavior, we can make a welcoming space for these critters rather than being their enemy.",
-	"sessionType": "keynote"
-},
-{
-	"title": "Recent Developments in Raccoon Chew-Toys",
-	"abstract": "Thanks to their opposable thumbs, raccoons are capable of enjoying chew toys that are significantly more complex than those made for cats and docs. We'll discuss how and why raccoons need more interesting toy designs, and what that means for current trends in chew toy manufacturing.",
-	"sessionType": "session"
-}
 `;
 
 export function createSessionPrompter(): (
