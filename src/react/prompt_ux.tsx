f339d4501a5b8f9cbf065e17f787b62092ef5667
nmsimons <nick@dreamlarge.com>
2024-09-20 15:03:52 -0700

    ui

--- src/react/prompt_ux.tsx
+++ src/react/prompt_ux.tsx
@@ -3,20 +3,28 @@ import React, { useState } from "react";
 export function HeaderPrompt(props: {
 	insertTemplate: (prompt: string) => Promise<void>;
 }): JSX.Element {
+	const placeholderType = "Type here to talk to a robot...";
+	const placeholderTalk = "Talking to a robot...";
+	const [promptText, setPromptText] = useState("");
 	const [templatePrompt, setTemplatePrompt] = useState("");
 	const [isLoadingTemplate, setIsLoadingTemplate] = useState(false);
+	const [buttonPrompt, setButtonPrompt] = useState("Talk");
+	const [placeholder, setPlaceholder] = useState(placeholderType);
+
 	return (
 		<div className="h-full w-full flex flex-row items-center gap-2">
 			<div className="flex h-fit w-full">
 				<textarea
-					placeholder="Type here to talk to a robot..."
+					disabled={isLoadingTemplate}
+					placeholder={placeholder}
 					rows={1}
 					style={{ resize: "none" }}
 					className="w-full bg-white text-black py-1 px-2 rounded-sm"
-					value={templatePrompt}
+					value={promptText}
 					id="insertTemplateInput"
 					aria-label="Describe the template to be inserted"
 					onChange={(e) => {
+						setPromptText(e.target.value);
 						setTemplatePrompt(e.target.value);
 					}}
 				/>
@@ -26,13 +34,25 @@ export function HeaderPrompt(props: {
 					className="bg-gray-500 hover:bg-gray-800 text-white font-bold w-20 h-full py-1 px-2 rounded"
 					id="insertTemplateButton"
 					onClick={() => {
-						setIsLoadingTemplate(true);
-						props.insertTemplate(templatePrompt).then(() => {
-							setIsLoadingTemplate(false);
-						});
+						if (isLoadingTemplate) {
+							// cancel the robot talk
+							return;
+						} else {
+							setIsLoadingTemplate(true);
+							setButtonPrompt("Cancel");
+							setPlaceholder(placeholderTalk);
+							setPromptText("");
+							console.log("Inserting template: " + templatePrompt);
+							props.insertTemplate(templatePrompt).then(() => {
+								setIsLoadingTemplate(false);
+								setButtonPrompt("Talk");
+								setPlaceholder(placeholderType);
+								setPromptText(templatePrompt);
+							});
+						}
 					}}
 				>
-					Talk
+					{buttonPrompt}
 				</button>
 			</div>
 		</div>
